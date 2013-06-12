4d32a0068829a8ffa265f18696ef4d6937120524
vinicius <vinicius@conectedesign.com.br>
2013-06-12 11:47:27 -0300

    Sticky Footer

--- home.php
+++ home.php
@@ -10,13 +10,26 @@
         <link rel="stylesheet" href="assets/css/main.css">
 
         <script src="assets/js/vendor/modernizr-2.6.2.min.js"></script>
+        <!--[if !IE 7]>
+        <style type="text/css">
+            #wrap {display:table;height:100%}
+        </style>
+        <![endif]-->
     </head>
     <body>
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
 
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <div id="wrap">
+            <header id="header"></header>
+            <!-- /#header -->
+            <div id="main"></div>
+            <!-- /#main -->
+        </div>
+        <!-- /#wrap -->
+        <div id="footer"></div>
+        <!-- /#footer -->
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
